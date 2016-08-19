59296db6f178ce2d0c12eb0db180140e35adb9b6
Ponomarenko Oleg <albka1986@mail.ru>
2016-08-20 01:05:54 +0300

    added testSaveProduct

--- src/test/java/DaoTests.java
+++ src/test/java/DaoTests.java
@@ -6,17 +6,38 @@ import ua.com.alfacell.dao.CrudDao;
 import ua.com.alfacell.dao.ProductDaoImpl;
 import ua.com.alfacell.models.Product;
 
-public class DaoTests {
+import java.util.List;
 
+public class DaoTests {
+    CrudDao<Product> productDao = new ProductDaoImpl();
     //ProductDaoImpl
     @Test
     public void testFindProductById() {
-        CrudDao<Product> productDao = new ProductDaoImpl();
         Product product = productDao.findById(1);
         Assert.assertNotNull(product);
         System.out.println(product);
+    }
+
+    @Test
+    public void testFindAllProducts() {
+        List<Product> products = productDao.findAll();
+        Assert.assertNotNull(products);
+        System.out.println(products);
+    }
+
+    @Test
+    public void testSaveProduct() {
+        Product product = new Product();
+        product.setBarcode("123123123123");
+        product.setBrand("Huawei");
+        product.setImei("123123123123123");
+        product.setNameProduct("y3c");
+
+        productDao.save(product);
+        System.out.println(productDao.findAll());
 
     }
+
 
 
 }
