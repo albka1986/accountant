86497ed8be05448418a0dff4fbf9fe8ca779ca96
Ponomarenko Oleg <albka1986@mail.ru>
2016-08-19 23:48:37 +0300

    add the method buildSessionFactory

---
+++ src/main/java/org/ponomarenko/utilites/HibernateUtil.java
@@ -0,0 +1,25 @@
+package org.ponomarenko.utilites;
+
+// Created by Ponomarenko Oleh on 07.06.2016.
+
+import com.fasterxml.classmate.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+}
