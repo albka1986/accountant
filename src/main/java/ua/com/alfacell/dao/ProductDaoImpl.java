ba9e0c22753f02cd9aa7f051422cfeda2c46764a
Ponomarenko Oleg <albka1986@mail.ru>
2016-08-20 00:25:15 +0300

    created ProductDaoImpl

---
+++ src/main/java/ua/com/alfacell/dao/ProductDaoImpl.java
@@ -0,0 +1,34 @@
+package ua.com.alfacell.dao;
+
+// Created by Ponomarenko Oleh on 20.08.2016.
+
+import ua.com.alfacell.models.Product;
+
+import java.util.List;
+
+public class ProductDaoImpl implements CrudDao<Product> {
+    @Override
+    public Product findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(Product product) {
+
+    }
+
+    @Override
+    public void update(Product product) {
+
+    }
+
+    @Override
+    public void delete(Product product) {
+
+    }
+}
