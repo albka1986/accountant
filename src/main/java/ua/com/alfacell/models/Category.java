199826e45e81214f0bf9443385ee4f5b12136002
Ponomarenko Oleg <albka1986@mail.ru>
2016-08-20 12:35:45 +0300

    create fields: id, nameCategory + annotation

---
+++ src/main/java/ua/com/alfacell/models/Category.java
@@ -0,0 +1,30 @@
+package ua.com.alfacell.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CATEGORIES")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String nameCategory;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameCategory() {
+        return nameCategory;
+    }
+
+    public void setNameCategory(String nameCategory) {
+        this.nameCategory = nameCategory;
+    }
+}
