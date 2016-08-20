42967b8130cf988c4796aa9cb118bd34a19e5c9c
Ponomarenko Oleg <albka1986@mail.ru>
2016-08-20 23:43:40 +0300

    overrode methods: CRUD

--- src/main/java/ua/com/alfacell/dao/impl/UserDaoImpl.java
+++ src/main/java/ua/com/alfacell/dao/impl/UserDaoImpl.java
@@ -2,6 +2,10 @@ package ua.com.alfacell.dao.impl;
 
 // Created by Ponomarenko Oleh on 20.08.2016.
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+import org.omg.CORBA.Request;
 import ua.com.alfacell.dao.BaseDao;
 import ua.com.alfacell.dao.CrudDao;
 import ua.com.alfacell.models.User;
@@ -9,28 +13,73 @@ import ua.com.alfacell.models.User;
 import java.util.List;
 
 public class UserDaoImpl extends BaseDao implements CrudDao<User> {
+
+
     @Override
     public User findById(int id) {
-        return null;
+        Session session = getActiveSession();
+        User user = (User) session.createCriteria(User.class)
+                .add(Restrictions.eq("id", id))
+                .uniqueResult();
+        return user;
     }
 
     @Override
     public List<User> findAll() {
-        return null;
+        Session session = getActiveSession();
+        List<User> users = session.createCriteria(User.class).list();
+        return users;
     }
 
     @Override
     public void save(User user) {
+        Session session = getActiveSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            session.save(user);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
 
     }
 
     @Override
     public void update(User user) {
+        Session session = getActiveSession();
+        Transaction tx = null;
 
+        try {
+            tx = session.beginTransaction();
+            session.save(user);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void delete(User user) {
+        Session session = getActiveSession();
+        Transaction tx = null;
 
+        try {
+            tx = session.beginTransaction();
+            session.delete(user);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 }
