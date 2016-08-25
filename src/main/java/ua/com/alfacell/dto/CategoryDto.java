ac5166d5506145fc0cbd9f8d36da9da61b2423e9
Ponomarenko Oleg <albka1986@mail.ru>
2016-08-25 23:37:52 +0300

    some changes

--- src/main/java/ua/com/alfacell/dto/CategoryDto.java
+++ src/main/java/ua/com/alfacell/dto/CategoryDto.java
@@ -1,7 +1,5 @@
 package ua.com.alfacell.dto;
 
-// Created by Ponomarenko Oleh on 25.08.2016.
-
 public class CategoryDto {
     private Integer id;
     private String nameCategory;
--- src/main/java/ua/com/alfacell/dto/RoleDto.java
+++ src/main/java/ua/com/alfacell/dto/RoleDto.java
@@ -1,7 +1,5 @@
 package ua.com.alfacell.dto;
 
-// Created by Ponomarenko Oleh on 25.08.2016.
-
 public class RoleDto {
     private String role;
 
