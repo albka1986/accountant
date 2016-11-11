b8a7fdd43826889ae84604b9c967402e06dc933e
Ponomarenko <albka1986@gmail.com>
2016-11-11 23:15:39 +0200

    working copy with three menu

--- src/main/java/ua/com/alfacell/servlet/ServletShop1.java
+++ src/main/java/ua/com/alfacell/servlet/ServletShop1.java
@@ -1,18 +1,12 @@
 package ua.com.alfacell.servlet;// Created by Ponomarenko Oleh on 05.11.2016.
 
-import ua.com.alfacell.dto.StorageDto;
-import ua.com.alfacell.models.Storage;
-import ua.com.alfacell.service.StorageService;
 import ua.com.alfacell.utilites.WebRedirect;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet("/shop1")
 public class ServletShop1 extends WebRedirect {
